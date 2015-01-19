5021ff7a330d6c6e3d3a143c04a85635b1203595
Alessio Rocco <alessio.rocco.lt@gmail.com>
2015-01-19 14:56:38 +0100

    removes some rubocop offenses

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,6 +1,8 @@
 Spree::Variant.class_eval do
-  has_and_belongs_to_many :spree_roles, join_table: 'spree_roles_variants',
-    foreign_key: 'variant_id', class_name: 'Spree::Role'
+  has_and_belongs_to_many :spree_roles,
+                          join_table: 'spree_roles_variants',
+                          foreign_key: 'variant_id',
+                          class_name: 'Spree::Role'
 
-  scope :exclusive_for, ->(spree_role_ids) { joins(:spree_roles).where( spree_roles: { id: spree_role_ids })}
+  scope :exclusive_for, -> (spree_role_ids) { joins(:spree_roles).where(spree_roles: { id: spree_role_ids }) }
 end
--- db/migrate/20141210094055_variants_to_roles.rb
+++ db/migrate/20141210094055_variants_to_roles.rb
@@ -1,5 +1,5 @@
 class VariantsToRoles < ActiveRecord::Migration
-  create_table :spree_roles_variants, :id => false do |t|
+  create_table :spree_roles_variants, id: false do |t|
     t.references :role
     t.references :variant
   end
